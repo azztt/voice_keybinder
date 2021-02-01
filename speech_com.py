580cbb434164a41194e9f9b44271f5c22e94a3fd
Anweshan Bor <ee3180878@iitd.ac.in>
2021-02-01 18:26:58 +0530

    module for recognizing and return text added

--- speech_com.py
+++ speech_com.py
@@ -1,22 +1,12 @@
 import speech_recognition as sr
-import os
 
-r = sr.Recognizer()
-m = sr.Microphone()
+def get_voice_command():
 
-# print("say now in 5 seconds")
-with m as source:
-    audio = r.listen(source, timeout=2000)
+    r = sr.Recognizer()
+    m = sr.Microphone()
 
-HOUNDIFY_CLIENT_ID = str(os.getenv('HOUNDIFY_CLIENT_ID'))
-HOUNDIFY_CLIENT_KEY = str(os.getenv('HOUNDIFY_CLIENT_KEY'))
-# text="jyhjhv"
-text = r.recognize_houndify(audio, client_id=HOUNDIFY_CLIENT_ID, client_key=HOUNDIFY_CLIENT_KEY)
-print("you probably said: " + text)
-# try:
-#     print("Houndify thinks you said " + r.recognize_houndify(audio, client_id=HOUNDIFY_CLIENT_ID, client_key=HOUNDIFY_CLIENT_KEY))
-# except sr.UnknownValueError:
-#     print("Houndify could not understand audio")
-# except sr.RequestError as e:
-#     print("Could not request results from Houndify service; {0}".format(e))
+    with m as source:
+        audio = r.listen(source, phrase_time_limit=1)
 
+    text = r.recognize_google(audio)
+    return text
