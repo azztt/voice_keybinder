6d381d3af9fe2eb7b93b7f5e4c712e4d23364ca8
shubham1206agra <tt1191044@iitd.ac.in>
2021-02-01 12:44:49 +0530

    keypress sim file added

--- key_events.py
+++ key_events.py
@@ -1,1 +1,13 @@
 # this file will contains modules for simulating key press events
+from pynput.keyboard import Key, Controller
+import time
+keyboar = Controller()
+
+def keypress(keys):
+    time.sleep(2)
+    if (len(keys) == 0): return
+    for key in keys:
+        keyboar.press(key)
+    time.sleep(2)
+    for key in keys[::-1]:
+        keyboar.release(key)
